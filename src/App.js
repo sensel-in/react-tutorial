b53bffd45f6beaa969fbf679a72b58bc92675488
Harikrishna R <r.harik@gmail.com>
2023-05-23 20:45:20 +0530

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const colStyle = {
+    borderStyle: 'solid',
+    borderWidth: '1px',
+    borderColor: 'red'
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container fluid>
+      <Row>
+        <MyCol sm={1}>Home</MyCol>
+        <MyCol>Header</MyCol>
+      </Row>
+      <Row>
+      <MyCol sm={1} lg={3} >Menu</MyCol>
+      <MyCol sm={10} lg={9 const colStyle = {
+    borderStyle: 'solid',
+    borderWidth: '1px',
+    borderColor: 'red'
+  }
+ }>Body</MyCol>
+      </Row>
+    </Container>
   );
 }
-
+function MyCol(props) {
+  const colStyle = {
+    borderStyle: 'solid',
+    borderWidth: '1px',
+    borderColor: 'red'
+  };
+  return (
+    <Col {...props} style={colStyle}>{props.children}</Col>
+  )
+}
 export default App;
