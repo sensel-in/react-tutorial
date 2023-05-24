c394ad4bd5a62af06fac374675740685c3cb2e6a
Harikrishna R <r.harik@gmail.com>
2023-05-25 01:17:57 +0530

    Component, data driven table

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import TripTable from './trip-table';
+
 import logo from './logo.svg';
 import './App.css';
 
@@ -12,30 +14,12 @@ function App() {
   }
   return (
     <Container fluid>
-      <Row>
-        <MyCol sm={1}>Home</MyCol>
-        <MyCol>Header</MyCol>
-      </Row>
-      <Row>
-      <MyCol sm={1} lg={3} >Menu</MyCol>
-      <MyCol sm={10} lg={9 const colStyle = {
-    borderStyle: 'solid',
-    borderWidth: '1px',
-    borderColor: 'red'
-  }
- }>Body</MyCol>
+      <Row className="justify-content-center">
+        <Col lg={8} >
+          <TripTable />
+        </Col>
       </Row>
     </Container>
   );
-}
-function MyCol(props) {
-  const colStyle = {
-    borderStyle: 'solid',
-    borderWidth: '1px',
-    borderColor: 'red'
-  };
-  return (
-    <Col {...props} style={colStyle}>{props.children}</Col>
-  )
 }
 export default App;
